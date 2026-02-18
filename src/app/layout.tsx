22cbaf56f1a87047d9d51463066b30ce00ebc0c4
Riobis <riobisofc@gmail.com>
2026-02-18 17:54:46 -0300

    feat(editor): install TipTap simple editor template

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
+import "@/styles/_variables.scss"
+import "@/styles/_keyframe-animations.scss"
 import Header from "@/components/header/Header"
 
 const geistSans = Geist({
