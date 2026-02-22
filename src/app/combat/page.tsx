81126b2dee4ce97b11e4d05d088876d6b3e62e38
Riobis <riobisofc@gmail.com>
2026-02-22 07:43:27 -0300

    Replace combat page with development placeholder

--- src/app/combat/page.tsx
+++ src/app/combat/page.tsx
@@ -1,15 +1,9 @@
 "use client"
 
-import { useState } from "react"
-import SelectCharacter from "./setup/SelectCharacter"
-import BattleScreen from "./battle/BattleController"
-import { BaseCharacter } from "@/types/BaseCharacter"
-
 export default function CombatPage() {
-  const [characters, setCharacters] = useState<BaseCharacter[] | null>(null)
-
-  if (!characters) {
-    return <SelectCharacter onConfirm={setCharacters} />
-  }
-  return <BattleScreen characters={characters} />
+  return (
+    <div>
+      <p>Em desenvolvimento...</p>
+    </div>
+  )
 }
