836ee086b58b5f4411e86c9cfef564d35ec6d985
Riobis <riobisofc@gmail.com>
2026-02-24 19:44:56 -0300

    chore(cleanup): remove unused legacy combat helpers

--- src/lib/turns/initTurnQueue.ts
+++
@@ -1,8 +0,0 @@
-import type { Turn } from "../../types/Turn"
-import { BaseCharacter } from "@/types/BaseCharacter"
-
-export default function initTurnQueue(characters: BaseCharacter[]): Turn[] {
-  return characters
-    .filter((c) => c.state.currentLife > 0)
-    .map((c) => ({ entity: c }))
-}
